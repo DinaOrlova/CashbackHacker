4bb5bd5faa2610f7cbd5db138e0137c5ab675e74
Dina Orlova <dina.turilova@gmail.com>
2021-01-14 16:38:05 +0300

    CashbackHackServiceTest changed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -56,7 +56,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
