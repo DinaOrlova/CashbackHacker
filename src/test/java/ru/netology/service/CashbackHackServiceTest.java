91fdd197328331890e0fc9f40de898e57a0d52c5
Dina Orlova <dina.turilova@gmail.com>
2021-01-14 17:07:18 +0300

    added Tests with JUnit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,8 @@
 package ru.netology.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -72,6 +75,83 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void remain700() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 6300;
+
+        int expected = 700;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainToMin1000j5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainToMin999j5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1;
+
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainToMin500j5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainToMin1j5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainToMin0j5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remain999j5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remain700j5() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 6300;
 
