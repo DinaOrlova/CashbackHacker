3ce0fe2c0a245dbe3f2b704a8f143a0c65df1ba0
Dina Orlova <dina.turilova@gmail.com>
2021-01-14 16:13:10 +0300

    added Tests with TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,85 @@
+package ru.netology.service;
+
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemainToMin1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainToMin999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainToMin500() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainToMin1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainToMin0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 5100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+}
